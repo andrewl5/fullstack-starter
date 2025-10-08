981d4b924bd1c910354e0c281f0f51d89b89dc8f
Andrew Lam <andrewlam542@gmail.com>
2025-10-08 10:07:12 -0400

    Task 2 done

--- starter/src/main/java/com/starter/fullstack/rest/InventoryController.java
+++ starter/src/main/java/com/starter/fullstack/rest/InventoryController.java
@@ -5,6 +5,8 @@ import com.starter.fullstack.dao.InventoryDAO;
 import java.util.List;
 import org.springframework.util.Assert;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,8 +20,10 @@ public class InventoryController {
 
   /**
    * Default Constructor.
+   *
    * @param inventoryDAO inventoryDAO.
    */
+
   public InventoryController(InventoryDAO inventoryDAO) {
     Assert.notNull(inventoryDAO, "Inventory DAO must not be null.");
     this.inventoryDAO = inventoryDAO;
@@ -27,11 +31,19 @@ public class InventoryController {
 
   /**
    * Find Products.
+   *
    * @return List of Product.
    */
+
   @GetMapping
   public List<Inventory> findInventories() {
     return this.inventoryDAO.findAll();
   }
-}
 
+  @PostMapping
+  public Inventory createInventory(@RequestBody Inventory inventory) {
+    return this.inventoryDAO.create(inventory);
+  }
+
+
+}
